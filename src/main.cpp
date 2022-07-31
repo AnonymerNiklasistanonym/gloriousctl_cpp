2373a2a10fb8caac67077d306a9b12fc317e5078
Niklas <niklas.mikeler@gmail.com>
2022-07-31 23:21:58 +0200

    Update project name and linking of HIDAPI library (on linux)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
-#include <cpp-template/app_version.hpp>
+#include <gloriousctl_cpp/app_version.hpp>
 
-int main() { std::cout << "cpp-template v" << cpp_template::version << std::endl; }
+#include <fcntl.h>
+#include <getopt.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <hidapi/hidapi.h>
+
+int main() {
+    std::cout << "cpp-template v" << cpp_template::version << std::endl;
+}
